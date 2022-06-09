74d566cda359bf19ff7945890115fd6ee5568992
Skorpito <106350740+Skorpito@users.noreply.github.com>
2022-06-08 22:46:14 -0700

    Add files via upload

--- ElfShooter/src/package01/Drawables.java
+++ ElfShooter/src/package01/Drawables.java
@@ -36,8 +36,8 @@ public class Drawables {
 class Elf extends Drawables{
 	private static int delay=250;
 	private static boolean canShoot=true;
-	private int bowXLoc = 800;
-	private int bowYLoc = 100;
+	private static int bowXLoc = 800;
+	private static int bowYLoc = 100;
 	
 	public boolean canShoot() {
 		return canShoot;
@@ -46,60 +46,151 @@ class Elf extends Drawables{
 	public void paint(Graphics2D g2d) {
 		 
 	}
-
-	public void shoot(int x, int y) {
-		ArrayList<Integer> removedDwarfs = new ArrayList<Integer>();
-		ArrayList<Dwarf> dwarfList = Dwarf.getDwarfList();
-		
-		for(int i=0;i<dwarfList.size();i++) {
-			 
-		}
+	
+	public static int getBowX() {
+		return bowXLoc;
+	}
+	public static int getBoyY() {
+		return bowYLoc;
 	}
 	
+	public void shoot(int x, int y) {
+		ArrayList<Integer> removedDwarfs = new ArrayList<Integer>();
+		ArrayList<Dwarf> dwarfList = Game.getDwarfList();
+		
+		//calculates which dwarfs need to be killed 
+		for(int i=0;i<dwarfList.size();i++) {
+			Dwarf dwarf=dwarfList.get(i);
+			 if(x==bowXLoc) { //to avoid issues with divide by zero
+				 if(dwarf.getXLoc()<=x && x<=dwarf.getXLoc()+dwarf.getDwarfWidth() && //between the x's of the hitbox 
+					y<=dwarf.getYLoc()) { //assuming that the dwarf is below the elf
+				System.out.println("HIT");
+				removedDwarfs.add(i);
+				 }
+			 }
+			 else {
+				//checks to see if it intersects the line for any of the dwarf's hitboxes
+				 
+				if(dwarf.getYLoc()<=calcVertIntersect(x, y, dwarf.getXLoc()) &&
+					calcVertIntersect(x, y, dwarf.getXLoc()) <= dwarf.getYLoc()+dwarf.getDwarfHeight()
+						){
+					removedDwarfs.add(i);
+					System.out.println("HIT");
+				} else if(dwarf.getYLoc()<=calcVertIntersect(x, y, dwarf.getXLoc()+dwarf.getDwarfWidth()) &&
+					calcVertIntersect(x, y, dwarf.getXLoc()+dwarf.getDwarfWidth()) <= dwarf.getYLoc()+dwarf.getDwarfHeight()
+						){
+					removedDwarfs.add(i);
+					System.out.println("HIT");
+					
+				} else if(dwarf.getXLoc()<=calcHorizontalIntersect(x, y, dwarf.getYLoc()) &&
+						calcHorizontalIntersect(x, y, dwarf.getYLoc())<=dwarf.getYLoc()+dwarf.getDwarfHeight()) {
+					removedDwarfs.add(i);
+					System.out.println("HIT");
+				} else if(dwarf.getXLoc()<=calcHorizontalIntersect(x, y, dwarf.getYLoc()+dwarf.getDwarfHeight()) &&
+						calcHorizontalIntersect(x, y, dwarf.getYLoc()+dwarf.getDwarfHeight())<=dwarf.getYLoc()+dwarf.getDwarfHeight()) {
+					removedDwarfs.add(i);
+					System.out.println("HIT");
+				} else {
+					System.out.println("MISS");
+				}
+			 }
+		}
+		
+		
+		for(int i= removedDwarfs.size()-1; i>=0; i--) {
+			dwarfList.add(new Dwarf(dwarfList.get(removedDwarfs.get(i))));
+			dwarfList.remove((int)removedDwarfs.get(i));
+		}
+		
+		Game.setDwarfList(dwarfList);
+	}
+	
+	/**
+	 * Calculates where the line given by the user intersects with a vertical line 
+	 * @param x 
+	 * @param y
+	 * @param lineLoc
+	 * @return Returns the vertical height of where the lines intersect
+	 */
 	public int calcVertIntersect(int x, int y, int lineLoc) {
-		int m = ( (bowYLoc-y)/(bowXLoc-x) );
-		int b = (y-m*x);
-		return m*lineLoc;
+		double m = ( (bowYLoc-y)/(bowXLoc-x) );
+		double b = (y-m*x);
+		return (int) (m*lineLoc + b);
+	}
+	public int calcHorizontalIntersect(int x, int y, int lineLoc) {
+		double m = ( ((double)bowYLoc-y)/(bowXLoc-x) );
+		double b = (y-m*x);
+		return (int) ((lineLoc-b)/m);
 	}
 }
 
 
 /**
  *
+ *
+ *
+ *
+ *
+ *
  */
-
 class Dwarf extends Drawables {
 	protected int xLoc;
 	protected int yLoc;
 	
 	private int xEnterLoc;
-	private int yEnterLoc;
+	private int yPathHeight;
 	
 	protected int xExitLoc;
-	protected int yExitLoc;
 	
-	private final int dwarfWidth=128;
-	private final int dwarfHeight=128;
+	private int dwarfWidth;
+	private int dwarfHeight;
 	
-	private static ArrayList <Dwarf> dwarfList = new ArrayList <Dwarf>();
 	private DwarfMovement m;
 	private Image img;
 	
-	public Dwarf () {
+	
+	
+	public Dwarf(int pathHeight, int xEnter, int xExit) {
 		String path = "src/images/dwarf.png";
 		img=(new ImageIcon(path)).getImage();
-		//m = new DwarfMovement();
-		//m.start();
-		//dwarfList.add(this);
+		dwarfHeight=img.getHeight(null);
+		dwarfWidth=img.getWidth(null);
+		
+		
+		yPathHeight=pathHeight;
+		
+		xEnterLoc=xEnter-dwarfWidth;
+		xExitLoc=xExit;
+		
+		xLoc=xEnterLoc;
+		yLoc=yPathHeight;
+		
+		m = new DwarfMovement();
+		m.start();
 	}
 	
-	public void setDwarfList(ArrayList<Dwarf> newDwarfList) {
-		dwarfList = newDwarfList;
-	}
-	public static ArrayList<Dwarf> getDwarfList(){
-		return dwarfList;
+	
+	public Dwarf(Dwarf dwarf) {
+		img=dwarf.getImg();
+		dwarfHeight=img.getHeight(null);
+		dwarfWidth=img.getWidth(null);
+		
+		xEnterLoc=dwarf.getXEnterLoc();
+		xExitLoc=dwarf.getXExitLoc();
+		
+		yPathHeight=dwarf.getY();
+		
+		xLoc=xEnterLoc;
+		yLoc=yPathHeight;
+		
+		m = new DwarfMovement();
+		m.start();
 	}
 
+	public Image getImg() {
+		return this.img;
+	}
+	
 	public int getXLoc () {
 		return (xLoc);
 	}
@@ -109,15 +200,13 @@ class Dwarf extends Drawables {
 	public int getXEnterLoc () {
 		return (xEnterLoc);
 	}
-	public int getYEnterLoc () {
-		return (yEnterLoc);
+	public int getY () {
+		return (yPathHeight);
 	}
 	public int getXExitLoc () {
 		return (xExitLoc);
 	}
-	public int getYExitLoc () {
-		return (yExitLoc);
-	}
+
 	public int getDwarfWidth () {
 		return (dwarfWidth);
 	}
@@ -126,28 +215,81 @@ class Dwarf extends Drawables {
 	}
 	
 	public void paint(Graphics2D g2d) {
-		g2d.drawImage(img,20,20,Game);
-		g2d.fillRect(0, 0, 20, 20);
+		g2d.drawImage(img,xLoc,yLoc,null);
 	}
 	
 	
+	
+	/**
+	 * Class that handles the dwarf moving to the right
+	 * 
+	 * @author ryanh
+	 *
+	 */
 	class DwarfMovement extends Thread {
+		/**
+		 * The amount that the dwarf moves per the delay
+		 */
+		private final int movePixels=4;
+		
+		/**
+		 * Delay until the dwarf moves
+		 */
+		private final int moveDelayMs=20;
+		
 		public void run () {
-			
+			while(true) {
+				xLoc=xLoc+movePixels;
+				if(xLoc>=xExitLoc) {
+					xLoc=xEnterLoc;
+				}
+				try {
+					sleep(moveDelayMs);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
 
+
 /**
-*
-*/
+ * 
+ * 
+ *
+ */
 class Background extends Drawables{
+	private Image img;
+	public Background () {
+		String path = "src/images/background.png";
+		img=(new ImageIcon(path)).getImage();
+}
 	public void paint(Graphics2D g2d) {
-		g2d.setColor(Color.GREEN);
-		g2d.fillRect(0, 0, Game.getWidth(), Game.getHeight());
-		g2d.setColor(Color.GRAY);
-		g2d.fillRect(Game.getWidth()-240, 0, Game.getWidth(), Game.getHeight());
-		g2d.fillRect(0, 300, Game.getWidth(), 20);
+		g2d.drawImage(img,0,0,null);
+	}
 		
+}
+
+
+/**
+ * Class that handles the arrow fired by the bow
+ * @author ryanh
+ *
+ */
+class Line extends Drawables{
+	private final int xStarting = Elf.getBowX();
+	private final int yStarting = Elf.getBoyY();
+	
+	private int x;
+	private int y;
+
+	public Line(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public void paint(Graphics2D g2d) {
+		g2d.drawLine(xStarting, yStarting, x, y);
 	}
 }
