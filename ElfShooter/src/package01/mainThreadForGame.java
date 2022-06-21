179c360bd2891f87f3146ceb59a39760a38b05df
Skorpito <106350740+Skorpito@users.noreply.github.com>
2022-06-21 13:44:33 -0700

    Update mainThreadForGame.java

--- ElfShooter/src/package01/mainThreadForGame.java
+++ ElfShooter/src/package01/mainThreadForGame.java
@@ -2,48 +2,61 @@ package package01;
 
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 /**
- * 
- * Based off of Java Minesweeper Game
+ * The basic setup for the game is based upon the minesweeper game by the person below
  *
  * Author: Jan Bodnar
  * Website: http://zetcode.com
+ * 
+ * Jframe allows for the drawing of the screen
  */
 public class mainThreadForGame extends JFrame {
 
     private JLabel statusbar;
     private GameSetupAndPlayer g;
     
+    //when you create the game class it just initializes the UI
     public mainThreadForGame() {
         initUI();
     }
 
+    
+    /**
+     * This code sets up the frame that contains the game 
+     */
     private void initUI() {
 
-    	//dont completely know what this does but its important I think sorta
-    	//sets up for drawing
+    	/*
+    	 * Add a status bar and the game to the window
+    	 */
         statusbar = new JLabel("");
         add(statusbar, BorderLayout.SOUTH);
         g=(new GameSetupAndPlayer(statusbar));
         add(g);
 		
+        /*
+         * Now make the window non-resizable and make the window fit the game
+         */
         setResizable(false);
         pack();
 
+        //Misc important setup
         setTitle("Java Test");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        	mainThreadForGame ex = new mainThreadForGame();
-            ex.setVisible(true);
+    	
+    	/*
+    	 * Create the game and make it visible
+    	 */
+    	mainThreadForGame ex = new mainThreadForGame();
+        ex.setVisible(true);
         
-        System.out.println("OK");
 
     }
 
