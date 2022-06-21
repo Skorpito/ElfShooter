264fd05704501b927a5892fa94d8b2e8a0a16150
Skorpito <106350740+Skorpito@users.noreply.github.com>
2022-06-21 13:45:20 -0700

    Update GameSetupAndPlayer.java

--- ElfShooter/src/package01/GameSetupAndPlayer.java
+++ ElfShooter/src/package01/GameSetupAndPlayer.java
@@ -15,23 +15,28 @@ public class GameSetupAndPlayer extends JPanel {
 
 	private ArrayList<Dwarf> dwarfList = new ArrayList<Dwarf>();
 	JLabel statusbar;
-	private Elf elf = new Elf();
+	private Elf elf= new Elf();
 	private Background b = new Background();
 	private Line line = new Line();
 
 	private int maxStreak=0;
 
-	// private JFrame testFrame =new JFrame("JFrame Color Example");
-
+	/**
+	 * Sets up the basic stuff required for the game
+	 * @param statusbar the status bar at the bottom
+	 */
 	public GameSetupAndPlayer(JLabel statusbar) {
+		//setup how big the screen is 
 		this.setSize(GAME_WIDTH, GAME_HEIGHT);
+		setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
 
+		
 		this.statusbar = statusbar;
-
-		elf = new Elf();
+		
+		//start up the game
 		newGame();
 		
-		
+		//this refreshes the screen 60 times per second in order to make the dwarf movement look smoth
 		screenRefreshHandler refresh = new screenRefreshHandler();
 		refresh.start();
 	}
@@ -49,14 +54,6 @@ public class GameSetupAndPlayer extends JPanel {
 	public void setDwarfList(ArrayList<Dwarf> newDwarf) {
 		dwarfList = newDwarf;
 	}
-	public void clearLine() {
-		line= new Line();
-	}
-	
-	public JLabel getStatusbar() {
-		return statusbar;
-	}
-	
 	
 	public int getMaxStreak() {
 		return maxStreak;
@@ -65,42 +62,57 @@ public class GameSetupAndPlayer extends JPanel {
 	public void setMaxStreak(int maxStreak) {
 		this.maxStreak = maxStreak;
 	}
+	
+	//This removes the line
+	public void clearLine() {
+		line= new Line(); //the constructor with no parameters makes the line invisible
+	}
+	
+	public JLabel getStatusbar() {
+		return statusbar;
+	}
+	
+	
+
 
 	/**
-	 * Contains the basic information needed for the game
+	 * Setup the basic things that the games relies on
 	 * 
 	 */
 	private void newGame() {
-		setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
+		//add he mouselistener that handles userinput
 		addMouseListener(new CardListener());
+		
+		//set the drawable's game to be this 
 		Drawables.setGame(this);
 		
-		
+		//add the two dwarfs
+		//adding the background and elf is not nessary, already add it
 		dwarfList.add(new Dwarf(50,0,440));
 		dwarfList.add(new Dwarf(GAME_HEIGHT-143-60,0,GAME_WIDTH));
+		
+		//Draw the graphics now that everything has been added 
 		repaint();
 		
 		
 	}
 
-	/**
-	 * 
-	 * okay this is a son of a female dog This is what is executed whenever you want
-	 * to paint something Can not have multiple seperate methods for separate things
-	 * Thus we go through the drawables arraylist and paint each one of those
-	 * instead to modify what is painted modify that
-	 */
 
+	/**
+	 * This is called whenever the repaint() method is called 
+	 * Note that there is only method so we must handle all the painting logic and order in here 
+	 */
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
 
-		// draw base sprites
+		// Change to be 2d as we are working in 2d
 		Graphics2D g2d = (Graphics2D) g.create();
 		
+		//draw the background (is first as the background is alwasys drawn over
 		b.paint(g2d);
-		elf.paint(g2d);
+		elf.paint(g2d); //not really needed the elf is never drawn
 	
 		
 		// draw all the dwarfs
@@ -108,9 +120,10 @@ public class GameSetupAndPlayer extends JPanel {
 			dwarf.paint(g2d);
 		}
 		
-		//will be null until
+		//paint the line being shot
 		line.paint(g2d);
 		
+		//prevent memory leak
 		g2d.dispose();
 	}
 
@@ -123,31 +136,46 @@ public class GameSetupAndPlayer extends JPanel {
 			int x = e.getX();
 			int y = e.getY();
 
+			//if it can shot shoot it
 			if (elf.getCanShoot()) {
+				
+				//print out diagnostic information
 				System.out.println(x +", " + y);
+				
+				//create a line that connects to the point being shot
 				line =new Line(x,y);
 				
+				//kill the dwarfs
 				elf.shoot(x, y);
+				
+				//make it enter a refactory period
 				elf.setCanShoot(false);
 				
+				//This runs in a seperate thread, makes sure that you cannot spam fire
 				RefreshHandler r= new RefreshHandler();
 				r.start();
 			}
 
+			//redraw everytime it  it is fired
 			repaint();
 			System.out.println("PRESSED");
 		}
 		
+		
+		//Makes it so that the elf can fire after some amount of time 
 		protected class RefreshHandler extends Thread{
 			private final int refreshTimeMs=500;
 			public void run () {
+				//This line of code is nessary to avoid a synchronization error
 				 synchronized(this){
+					//try catch is nessary for the wait method
 					try {wait(refreshTimeMs);}
 					catch (InterruptedException e) {e.printStackTrace();}
 					
 					if(!elf.getCanShoot()) {
 						elf.setCanShoot(true);
 						}
+					//remove the line to indicate to the user that they can shoot again
 					clearLine();
 				 }
 			}
@@ -156,22 +184,23 @@ public class GameSetupAndPlayer extends JPanel {
 	
 	
 	/**
-	 * 
-	 *
+	 * The class that handles how long it takes before the screen refreshes,  in a new thread
 	 */
 	class screenRefreshHandler extends Thread{
 		/**
 		 * Refresh the screen 60 times a second
 		 */
 		public void runScreenRefesher() {
+			//This line of code is nessary to avoid a synchronization error
 			synchronized(this) {
 				while(true) {
+					//try catch is nessary for the wait method
 					try {
 						wait(1000/60);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					
+					//redraw the screen
 					repaint();
 				}
 			}
@@ -180,17 +209,21 @@ public class GameSetupAndPlayer extends JPanel {
 		
 		/**
 		 * Refresh the screen every some amount of milliseconds
+		 * Not used currently but can be added for debugging
 		 * @param waitTimeMs The amount of time that the refresh handler will wait before refreshing the screen 
 		 */
 		public void runScreenRefesher(int waitTimeMs) {
+			//This line of code is nessary to avoid a synchronization error
 			synchronized(this) {
 				while(true) {
+					//try catch is nessary for the wait method
 					try {
 						wait(waitTimeMs);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 					
+					//redraw the screen
 					repaint();
 				}
 			}
@@ -202,4 +235,4 @@ public class GameSetupAndPlayer extends JPanel {
 
 
 
-}
+}	
